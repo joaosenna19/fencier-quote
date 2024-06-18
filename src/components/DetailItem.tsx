0349c3a49295587e5828fdd2e43d2c1a18a316cb
joaosenna19 <101044996+joaosenna19@users.noreply.github.com>
2024-06-18 16:48:59 -0600

    Refactors quote summary display after fetching more info

---
+++ src/components/DetailItem.tsx
@@ -0,0 +1,11 @@
+function DetailItem(props: any) {
+  const { label, value } = props;
+  return (
+    <p className="text-sm font-medium leading-none flex flex-col mb-3 mx-2">
+      {label}
+      <span className="text-3xl font-bold mt-2">{value}</span>
+    </p>
+  );
+}
+
+export default DetailItem;
